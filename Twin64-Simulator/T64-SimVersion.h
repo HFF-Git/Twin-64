fb52b644f5a7b995e4e87ac06f420fadbaca041c
Helmut Fieres <67054756+HFF-Git@users.noreply.github.com>
2025-12-21 12:16:43 +0100

    Next test

--- Twin64-Simulator/T64-SimVersion.h
+++ Twin64-Simulator/T64-SimVersion.h
@@ -30,7 +30,7 @@
 
 const char SIM_VERSION[ ] = "A.00.01";
 const char SIM_GIT_BRANCH[ ] = "main";
-const int  SIM_PATCH_LEVEL = 3;
+const int  SIM_PATCH_LEVEL = 4;
 
 #if __APPLE__
 const bool SIM_IS_APPLE = true;
